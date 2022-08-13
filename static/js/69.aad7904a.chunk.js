dba1c9b74f4a66c331b664778ae0e7cae1bcfeaa
Volodymyr Viter <mad.tvido@gmail.com>
2022-08-13 21:30:04 +0300

    Updates

---
+++ static/js/69.aad7904a.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpliceum20=this.webpackJsonpliceum20||[]).push([[69],{581:function(e,c,n){"use strict";n.r(c);var s=n(0),t=n.n(s),i=n(1);c.default=function(){return Object(i.jsx)(t.a.Fragment,{children:Object(i.jsx)("div",{className:"main__section"})})}}}]);
+//# sourceMappingURL=69.aad7904a.chunk.js.map
