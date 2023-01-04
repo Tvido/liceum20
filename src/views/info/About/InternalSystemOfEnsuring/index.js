da9377be4cf1a0093984d7c74382dfe5b4e95c11
stror10 <str.or10@gmail.com>
2023-01-04 18:14:26 +0200

    Internal system of Ensuring  update

--- src/views/info/About/InternalSystemOfEnsuring/index.js
+++ src/views/info/About/InternalSystemOfEnsuring/index.js
@@ -4,6 +4,32 @@ const InternalSystemOfEnsuring = () => {
   return (
     <React.Fragment>
       <div className="article__wrapper">
+
+      <div className="section__grid">
+        <a target="_blank" rel="noreferrer" href="https://docs.google.com/document/d/1nuzYWgEDWGTuyjHHHp1rv_6FQhPlM741/edit?usp=share_link&ouid=104991853798134703900&rtpof=true&sd=true">
+          <div className="section__box">
+            <div className="section__content">
+              <h3>Клікни, щоби читати</h3>
+
+              <p>Положення про внутрішню систему забезпечення якості освіти 2023</p>
+            </div>
+          </div>
+        </a>
+
+
+        <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/14ZbCY48IhXJT92Wia0fgZDprFmZCBj6Z/view" rel="noreferrer">
+          <div className="section__box">
+            <div className="section__content">
+              <h3>Клікни, щоби читати</h3>
+
+              <p>Положення про внутрішню систему забезпечення якості освіти 2020</p>
+            </div>
+          </div>
+        </a>
+      </div>
+
+      {/*
+
       <div className="terms__link">
         <a target="_blank" href="https://drive.google.com/file/d/14ZbCY48IhXJT92Wia0fgZDprFmZCBj6Z/view" rel="noreferrer">
           Читати увесь документ
@@ -96,7 +122,11 @@ const InternalSystemOfEnsuring = () => {
           </ul>
         </article>
       </div>
+
+*/}
         </div>
+
+  
 
     </React.Fragment>
   );
