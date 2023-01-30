11caad3a96202a4875d73472fb63b66953261221
stror10 <str.or10@gmail.com>
2023-01-30 16:48:03 +0200

    main-video scss

--- src/views/main/Main/index.js
+++ src/views/main/Main/index.js
@@ -1,9 +1,50 @@
 import React from 'react';
 
+
 const Main = () => {
   return (
     <React.Fragment>
-      <div className="main__section"></div>
+      <div className="main__section">
+      {/* <div className="section__grid">
+        <a target="_blank" rel="noreferrer" href="https://docs.google.com/document/d/1nuzYWgEDWGTuyjHHHp1rv_6FQhPlM741/edit?usp=share_link&ouid=104991853798134703900&rtpof=true&sd=true">
+         
+        </a>
+
+
+        <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/14ZbCY48IhXJT92Wia0fgZDprFmZCBj6Z/view" rel="noreferrer">
+          
+        </a>
+      </div>
+       <div className="section__grid">
+        <a target="_blank" rel="noreferrer" href="https://docs.google.com/document/d/1nuzYWgEDWGTuyjHHHp1rv_6FQhPlM741/edit?usp=share_link&ouid=104991853798134703900&rtpof=true&sd=true">
+         
+        </a>
+
+
+        <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/14ZbCY48IhXJT92Wia0fgZDprFmZCBj6Z/view" rel="noreferrer">
+          
+        </a>
+      </div>
+      <div className="section__grid">
+       <a target="_blank" rel="noreferrer" href="#">
+           {/* <div className="section__box">
+            <div className="section__content">
+                </div>
+          </div> 
+        </a>
+
+
+       
+      </div> */}
+
+      
+         
+        
+
+    
+<iframe className="main-video"  src="https://www.youtube.com/embed/PTvQIJ8clCA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      </div>
+      
     </React.Fragment>
   );
 };
