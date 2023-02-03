a8de13f7a8268017aba0b02f1d69ba4161ce0116
stror10 <str.or10@gmail.com>
2023-02-03 13:23:37 +0200

    Estimates and Financial Staements update

--- src/views/main/EstimatesAndFinancialStatements/index.js
+++ src/views/main/EstimatesAndFinancialStatements/index.js
@@ -15,6 +15,17 @@ const EstimatesAndFinancialStatements = () => {
             </div>
           </div>
         </a>
+      
+
+      <a target="_blank" rel="noreferrer" href="https://docs.google.com/document/d/1fWSvq3Ysd2MI5jCIyOsrXG8k_S-q5ny0/edit?usp=share_link&ouid=104991853798134703900&rtpof=true&sd=true">
+          <div className="section__box">
+            <div className="section__content">
+              <h3>Клікни, щоби читати</h3>
+
+              <p>Обґрунтування технічних та якісних характеристик закупівлі, розміру бюджетного призначення, очікуваної вартості предмета закупівлі – електрична енергія</p>
+            </div>
+          </div>
+        </a>
       </div>
 
 
