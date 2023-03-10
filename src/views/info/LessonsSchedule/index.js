2c8d69186e9607d4de1d2ad0ecbef53f10df77bf
stror10 <str.or10@gmail.com>
2023-03-10 14:52:07 +0200

    Orders? LessonsSchedule, EducationalPrograms update

--- src/views/info/LessonsSchedule/index.js
+++ src/views/info/LessonsSchedule/index.js
@@ -259,7 +259,7 @@ const LessonsSchedule = () => {
 
           <Tabs variant="pills" defaultActiveKey="schedule" className="mb-3">
             <Tab eventKey="schedule" title="Розклад дзвінків">
-              <div className="terms__link">
+              {/* <div className="terms__link">
                 <a target="_blank" href="https://drive.google.com/file/d/1OqgPRcXzQi5kw4XvBxreBpaN2qOlPtRl/view" rel="noreferrer">
                   Розклад I ступінь 2020-2021 (початкова школа)        
                 </a>
@@ -269,7 +269,7 @@ const LessonsSchedule = () => {
                 <a target="_blank" href="https://docs.google.com/document/d/1vxBpLmNxBfoFtIfWEzyMV0kewIRMT0sp/edit?rtpof=true&sd=true" rel="noreferrer">
                   Розклад II та III ступенів 2020-2021        
                 </a>
-              </div>
+              </div> */}
 
               <Card.Body>
                 <Table responsive hover>
@@ -312,86 +312,87 @@ const LessonsSchedule = () => {
                 <Table responsive hover>
                 <tbody>
                   <tr>
-                    <th scope="row">1 класи</th>
+                    <th scope="row"></th>
+                    <th>1 класи</th>
                     <th>2 - 4 класи</th>
                     <th>5 - 11 класи</th>
-                    <th></th>
+                    
                   </tr>
 
                   <tr>
                     <th scope="row">1 урок</th>
-                    <th>8:00 – 8:35</th>
-                    <th>8:00 – 8:35</th>
+                    <th>8:30 – 9:05</th>
+                    <th>8:30 – 9:10</th>
                     <th>8:30 - 9:15</th>
                   </tr>
 
                   <tr>
                     <th scope="row">Перерва</th>
-                    <th>8:35 – 8:50 (15хв)</th>
-                    <th>8:40 - 8:50 (10хв)</th>
+                    <th>9:05 – 9:20 (15хв)</th>
+                    <th>9:10 - 9:20 (10хв)</th>
                     <th>9:15 – 9:25 (10хв)</th>
                   </tr>
 
                   <tr>
                     <th scope="row">2 урок</th>
-                    <th>8:50 – 9:25</th>
-                    <th>8:50 – 9:30</th>
+                    <th>9:20 – 9:55</th>
+                    <th>9:20 – 10:00</th>
                     <th>9:25 - 10:10</th>
                   </tr>
 
                   <tr>
                     <th scope="row">Перерва</th>
-                    <th>9:25 - 9:50 (25хв)</th>
-                    <th>9:30 – 9:50 (20хв)</th>
+                    <th>9:55 - 10:15 (20хв)</th>
+                    <th>10:00 – 10:20 (20хв)</th>
                     <th>10:10 - 10:30 (20хв)</th>
                   </tr>
 
                   <tr>
                     <th scope="row">3 урок</th>
-                    <th>9:50 – 10:25</th>
-                    <th>9:50 – 10:30</th>
+                    <th>10:15 – 10:50</th>
+                    <th>10:20 – 11:00</th>
                     <th>10:30 – 11:15</th>
                   </tr>
 
                   <tr>
                     <th scope="row">Перерва</th>
-                    <th>10:25 – 10:50 (25хв)</th>
-                    <th>10:30 - 105:0 (20хв)</th>
+                    <th>10:50 – 11:10 (20хв)</th>
+                    <th>11:00 - 11:20 (20хв)</th>
                     <th>11:15 - 11:35 (20хв)</th>
                   </tr>
 
                   <tr>
                     <th scope="row">4 урок</th>
-                    <th>10:50 – 11:25</th>
-                    <th>10:50 – 11:30</th>
+                    <th>11:10 – 11:45</th>
+                    <th>11:20 – 12:00</th>
                     <th>11:35 - 12:20</th>
                   </tr>
 
                   <tr>
                     <th scope="row">Перерва</th>
-                    <th>11:25 – 11:40 (15хв)</th>
-                    <th>11:30 – 11:50 (20 хв)</th>
+                    <th>11:45 – 12:00 (15хв)</th>
+                    <th>12:00 – 12:10 (10хв)</th>
                     <th>12:20 – 12:40 (20хв)</th>
                   </tr>
 
                   <tr>
                     <th scope="row">5 урок</th>
-                    <th>11:40 - 12:15</th>
-                    <th>11:50 – 12:30</th>
+                    <th>12:00 - 12:35</th>
+                    <th>12:10 – 12:50</th>
                     <th>12:40 – 13:25</th>
                   </tr>
 
                   <tr>
                     <th>Перерва</th>
                     <th></th>
-                    <th>12:30 – 12:40 (10хв)</th>
+                    <th>12:50 – 13:00 (10хв)</th>
                     <th>13:25 – 13:35 (10хв)</th>
                   </tr>
 
                   <tr>
                     <th>6 урок</th>
                     <th></th>
-                    <th>12:40 – 13:20</th>
+                    <th>13:00 – 13:40</th>
                     <th>13:35 - 14:20</th>
                   </tr>
 
