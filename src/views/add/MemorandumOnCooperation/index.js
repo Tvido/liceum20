d462acad5bd1d3954d31cb3986f3ee0ff585d5c0
stror10 <str.or10@gmail.com>
2023-03-15 11:16:53 +0200

    Memorandum update

--- src/views/add/MemorandumOnCooperation/index.js
+++ src/views/add/MemorandumOnCooperation/index.js
@@ -5,7 +5,17 @@ const MemorandumOnCooperation = () => {
     <React.Fragment>
       <div className="article__wrapper">
         <div className="section__grid">
-          <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1V2lQFwfax7L5rukqCTxepA0y2R6UlLCv/view">
+          {/* <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1V2lQFwfax7L5rukqCTxepA0y2R6UlLCv/view">
+            <div className="section__box">
+              <div className="section__content">
+                <h3>Клікни, щоби читати</h3>
+
+                <p>Меморандум про співпрацю</p>
+              </div>
+            </div>
+          </a> */}
+
+          <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/12wDdyZuFe0YczAcS2NOsEzlpVSyeHrOR/view?usp=share_link">
             <div className="section__box">
               <div className="section__content">
                 <h3>Клікни, щоби читати</h3>
