b397cbe7bd7b7b352a328bb9ad789aea87a283d6
stror10 <str.or10@gmail.com>
2023-03-20 10:01:54 +0200

    CharityFund update

--- src/views/add/CharityFund/index.js
+++ src/views/add/CharityFund/index.js
@@ -56,6 +56,26 @@ const CharityFund = () => {
                 </div>
               </div>
             </a>
+
+            <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/10CXFkcRTEVtiQFCyo49tb0T9maG1OI4T/view?usp=share_link">
+              <div className="section__box">
+                <div className="section__content">
+                  <h3>Клікни, щоби читати</h3>
+
+                  <p>“Благодійний фонд НВК “Школа-гімназія №3” за період 2020-2021р.</p>
+                </div>
+              </div>
+            </a>
+
+            <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1q9SZDQUe9UzixjeFcr7YazudNvFxPekU/view?usp=share_link">
+              <div className="section__box">
+                <div className="section__content">
+                  <h3>Клікни, щоби читати</h3>
+
+                  <p>"Благодійна організація "Благодійний фонд "ЗГАРДА" за період 2021-2022р.</p>
+                </div>
+              </div>
+            </a>
           </div>
         </div>
     </React.Fragment>
