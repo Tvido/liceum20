61a9548b62c12e90187416545efe68b8bbde9b09
stror10 <str.or10@gmail.com>
2023-03-21 15:51:19 +0200

    MaterialAndTechnicalSupport update

--- src/views/info/About/MaterialAndTechnicalSupport/index.js
+++ src/views/info/About/MaterialAndTechnicalSupport/index.js
@@ -14,6 +14,16 @@ const MaterialAndTechnicalSupport = () => {
             </div>
           </div>
         </a>
+
+         <a target="_blank" rel="noreferrer" href="https://docs.google.com/document/d/1mCGsgBcJqKSr5HEr67KGl_d90li3HmFc/edit?usp=share_link&ouid=104991853798134703900&rtpof=true&sd=true">
+          <div className="section__box">
+            <div className="section__content">
+              <h3>Клікни, щоби читати</h3>
+
+              <p>Матеріально-технічне забезпечення 2020-2023н.р.</p>
+            </div>
+          </div>
+        </a>
       </div>
         </div>
 
