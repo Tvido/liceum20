f7f43cf7eb24ac79be5eaacea1c595bd48f1e615
stror10 <str.or10@gmail.com>
2023-06-08 10:30:37 +0300

    Estimates update

--- src/views/main/EstimatesAndFinancialStatements/index.js
+++ src/views/main/EstimatesAndFinancialStatements/index.js
@@ -430,6 +430,17 @@ const EstimatesAndFinancialStatements = () => {
             </div>
           </div>
         </a>
+
+
+        <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1LMwWHwq1plUIhG1G87e5XmFeuZmsFDy3/view?usp=sharing">
+          <div className="section__box">
+            <div className="section__content">
+              <h3>Клікни, щоби читати</h3>
+
+              <p>Звіт про обіг бюджетних коштів за травень 2023 року</p>
+            </div>
+          </div>
+        </a>
       </div>
         </div>
 
