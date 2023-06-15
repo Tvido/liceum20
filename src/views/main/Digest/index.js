902e706c44a2c07cc0912b88d0adee4f3d821917
stror10 <str.or10@gmail.com>
2023-06-15 19:42:54 +0300

    Digest update

--- src/views/main/Digest/index.js
+++ src/views/main/Digest/index.js
@@ -31,7 +31,38 @@ const Digest = () => {
     <React.Fragment>
       <div className="article__wrapper">
 
-      
+      <div className="cardWrapper">
+          <Col md={12}>
+            <Card>
+              <Card.Header>
+                <Card.Title as="h5">15.06.2023</Card.Title>
+              </Card.Header>
+              <Card.Body>
+             
+              <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1jvO5odgD3uQiCtN91RTdZPtG8ehNHHgu/view?usp=sharing">
+                  <div className="section__box">
+                    <div className="section__content">
+                      <h3>Клікни, щоби читати</h3>
+
+                      <p>Про зарахування здобувачів освіти на вільні місця до 1-ого класу на 2023-2024 н.р.</p>
+                    </div>
+                  </div>
+                </a>
+                
+              <br></br>
+
+            
+                                   
+
+                <blockquote className="blockquote text-right">
+                  <footer className="blockquote-footer">
+                  <cite title="Source Title">Наказ про зарахування здобувачів освіти на вільні місця до 1-ого класу на 2023-2024 н.р.</cite>
+                  </footer>
+                </blockquote>
+              </Card.Body>
+            </Card>
+          </Col>
+      </div>
 
         <div className="cardWrapper">
           <Col md={12}>
