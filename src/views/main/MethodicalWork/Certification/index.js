a2c52f6d44e27045968c94ba26fd14963562b9d0
stror10 <str.or10@gmail.com>
2023-10-13 11:35:38 +0300

    Certification update

--- src/views/main/MethodicalWork/Certification/index.js
+++ src/views/main/MethodicalWork/Certification/index.js
@@ -80,7 +80,18 @@ const Certification = () => {
             </div>
           </div>
         </a>
+
+        <a target="_blank" rel="noreferrer" href=" https://docs.google.com/document/d/114KTQewg0P4w42SR9KBLowIalvNGcqRK/edit?usp=sharing&ouid=104991853798134703900&rtpof=true&sd=true">
+          <div className="section__box">
+            <div className="section__content">
+              <h3>Клікни, щоби читати</h3>
+
+              <p>Список педагогічних працівників Ліцею №20, які атестуються у 2024</p>
+            </div>
+          </div>
+        </a>
       </div>
+     
         </div>
 
     </React.Fragment>
