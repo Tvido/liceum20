52cf59bf6776ebfe7043fda3d176a3aa9151cdfa
stror10 <str.or10@gmail.com>
2023-11-17 12:23:43 +0200

    AnnualPlan & CareerGuidanceWork update

--- src/views/main/AnnualPlan/index.js
+++ src/views/main/AnnualPlan/index.js
@@ -11,6 +11,16 @@ const AnnualPlan = () => {
 
 
       <div className="article__wrapper">
+      <div className="section__grid">
+      <a target="_blank" rel="noreferrer" href="https://docs.google.com/document/d/19w9Wg_gx8RDBPbZdRe9MDbjaJFQOXfxq/edit?usp=sharing&ouid=104991853798134703900&rtpof=true&sd=true">
+          <div className="section__box">
+            <div className="section__content">
+              <h3>Клікни, щоби читати</h3>
+
+              <p>Річний план роботи ліцею на 2023-2024 н. р.</p>
+            </div>
+          </div>
+        </a>
 
       <a target="_blank" rel="noreferrer" href="https://docs.google.com/document/d/1TYPl9Q5AH2AvlFEHpjE38g46YOz2zRJd/edit?usp=sharing&ouid=104991853798134703900&rtpof=true&sd=true">
           <div className="section__box">
@@ -21,7 +31,7 @@ const AnnualPlan = () => {
             </div>
           </div>
         </a>
-
+    </div>
       <h5>РІЧНИЙ ПЛАН Ліцею №20 Івано-Франківської міської ради Івано-Франківської області на 2020/2021 навчальний рік</h5>
 
       <div className="terms__link">
--- src/views/main/EducationalWork/CareerGuidanceWork/index.js
+++ src/views/main/EducationalWork/CareerGuidanceWork/index.js
@@ -6,6 +6,75 @@ import one from './one.jpg'
 const CareerGuidanceWork = () => {
   return (
     <React.Fragment>
+
+<div className="article__wrapper">
+    <div className="section__grid">
+      <a target="_blank" rel="noreferrer" href="https://youtu.be/1YWWQgXp20g">
+          <div className="section__box">
+            <div className="section__content">
+              <h3>Клікни, щоби читати</h3>
+
+              <p>Харківський національний університет Повітряних Сил імені Івана Кожедуба</p>
+            </div>
+          </div>
+        </a>
+
+      <a target="_blank" rel="noreferrer" href="https://youtu.be/mx9A_0ABMw4">
+          <div className="section__box">
+            <div className="section__content">
+              <h3>Клікни, щоби читати</h3>
+
+              <p>ВІТАЄМО З ДНЕМ ПОВІТРЯНИХ СИЛ ЗБРОЙНИХ СИЛ УКРАЇНИ</p>
+            </div>
+          </div>
+        </a>
+    
+
+    
+      <a target="_blank" rel="noreferrer" href="https://youtu.be/qQtQfaR4KOE">
+          <div className="section__box">
+            <div className="section__content">
+              <h3>Клікни, щоби читати</h3>
+
+              <p>Факультет ХНУПС ім. Івана Кожедуба</p>
+            </div>
+          </div>
+        </a>
+
+      <a target="_blank" rel="noreferrer" href="https://youtu.be/K_jjKRtNXGA">
+          <div className="section__box">
+            <div className="section__content">
+              <h3>Клікни, щоби читати</h3>
+
+              <p>ХНУПС ім. Івана Кожедуба презентує</p>
+            </div>
+          </div>
+        </a>
+    
+
+    
+      <a target="_blank" rel="noreferrer" href="https://docs.google.com/presentation/d/1XM12iP6GFsKGGOLzhD7z8XpInCt5DdNS/edit?usp=sharing&ouid=104991853798134703900&rtpof=true&sd=true">
+          <div className="section__box">
+            <div className="section__content">
+              <h3>Клікни, щоби читати</h3>
+
+              <p>ХНУПС ім. Івана Кожедуба. Агітація </p>
+            </div>
+          </div>
+        </a>
+
+      <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1k24irqABkaLgDfsTS-B_M93xjQ6-o5ru/view?usp=sharing">
+          <div className="section__box">
+            <div className="section__content">
+              <h3>Клікни, щоби читати</h3>
+
+              <p>Інженерно-авіаційний факультет ХНУПС запрошує до вступу 2023 році</p>
+            </div>
+          </div>
+        </a>
+    </div>
+    </div>
+
             <div className="article__wrapper">
         <p>Головною потребою глобалізованого світу у XXI столітті є комунікація. Спілкування та взаємодія між глобальними суб’єктами, країнами потребує висококласних фахівців з перекладу. Перекладач — професія, яка не втрачає актуальності. Перекладачі завжди потрібні в консульствах та міністерствах, бізнесі та на виробництвах, у торгових мережах і маленьких фірмах і, звичайно, видавництвах.</p>
         <p>Отримавши диплом перекладача, ти зможеш реалізувати себе в багатьох сферах: від перекладу важливих міжнародних переговорів до перекладу художніх текстів чи роботи над адаптацією голлівудських фільмів.</p>
