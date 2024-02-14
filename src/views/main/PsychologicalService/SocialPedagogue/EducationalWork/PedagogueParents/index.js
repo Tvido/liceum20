988359b949c8af3b293d96e004acc4d6c37ea039
stror10 <str.or10@gmail.com>
2024-02-14 18:14:30 +0200

    PedagogueParents update

--- src/views/main/PsychologicalService/SocialPedagogue/EducationalWork/PedagogueParents/index.js
+++ src/views/main/PsychologicalService/SocialPedagogue/EducationalWork/PedagogueParents/index.js
@@ -6,6 +6,16 @@ const PedagogueParents = () => {
       <div className="article__wrapper">
       <div className="section__grid">
 
+      <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1PZ4fUDm_tCK40ANpUU_Pj3_ckoZX7804/view?usp=sharing">
+          <div className="section__box">
+            <div className="section__content">
+              <h3>Клікни, щоби читати</h3>
+
+              <p>Порядок надання статусу дитини, яка постраждала від воєнних дій та збойних конфліктів</p>
+            </div>
+          </div>
+        </a>
+
       <a target="_blank" rel="noreferrer" href="https://docs.google.com/document/d/1FP7dCw7CwOfTACLAUcOQKF53qG8P1_NP/edit?usp=sharing&ouid=104991853798134703900&rtpof=true&sd=true">
           <div className="section__box">
             <div className="section__content">
