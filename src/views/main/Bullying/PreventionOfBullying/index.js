07bd50f2496ab210affb4e089aca46d173e7a1a7
stror10 <str.or10@gmail.com>
2024-03-05 09:04:27 +0200

    Bullying update

--- src/views/main/Bullying/PreventionOfBullying/index.js
+++ src/views/main/Bullying/PreventionOfBullying/index.js
@@ -27,6 +27,46 @@ const PreventionOfBullying = () => {
           </div>
         </a>
 
+        <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1g03m4Ea4b9-x759J3A8Y799nQmhK5CDm/view?usp=sharing">
+          <div className="section__box">
+            <div className="section__content">
+              <h3>Клікни, щоби читати</h3>
+
+              <p>Протидія булінгу</p>
+            </div>
+          </div>
+        </a>
+
+        <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1zR2-E5dz0zNWJIR1d-0KL_S2XDrV8c6K/view?usp=sharing">
+          <div className="section__box">
+            <div className="section__content">
+              <h3>Клікни, щоби читати</h3>
+
+              <p>Якщо ваша дитина агресор або якщо ваша дитина стала жертвою булінгу</p>
+            </div>
+          </div>
+        </a>
+
+        <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1Q8zoKmk9Z5CAVqD9bvKBQwQGwwwaGvNu/view?usp=sharing">
+          <div className="section__box">
+            <div className="section__content">
+              <h3>Клікни, щоби читати</h3>
+
+              <p>Порядок реагування на доведені випадки булінгу</p>
+            </div>
+          </div>
+        </a>
+
+        <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1pvCu4zmy5opZRXy_f6Ye0_lHVmDOOxXq/view?usp=sharing">
+          <div className="section__box">
+            <div className="section__content">
+              <h3>Клікни, щоби читати</h3>
+
+              <p>Комісія і засідання</p>
+            </div>
+          </div>
+        </a>
+
       
       </div>
         </div>
