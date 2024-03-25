2d9b1150f52fad16cd84c999b6f185d158054c0a
stror10 <str.or10@gmail.com>
2024-03-25 15:51:03 +0200

    CharityFund update

--- src/views/add/CharityFund/index.js
+++ src/views/add/CharityFund/index.js
@@ -76,6 +76,17 @@ const CharityFund = () => {
                 </div>
               </div>
             </a>
+
+            <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1-LzdcX9xCnV0aWrakUIlrp5fu3Qy-HlH/view?usp=sharing">
+              <div className="section__box">
+                <div className="section__content">
+                  <h3>Клікни, щоби читати</h3>
+
+                  <p>"Благодійна організація "Благодійний фонд "ЗГАРДА" за період 2023р.</p>
+                </div>
+              </div>
+            </a>
+
           </div>
         </div>
     </React.Fragment>
