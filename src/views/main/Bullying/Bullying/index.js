cf7ff482001af27859a1ef446f3d0d9d827fb882
stror10 <str.or10@gmail.com>
2024-03-26 13:29:37 +0200

    Bullying update

--- src/views/main/Bullying/Bullying/index.js
+++ src/views/main/Bullying/Bullying/index.js
@@ -9,6 +9,19 @@ const Bullying = () => {
   return (
     <React.Fragment>
       <div className="article__wrapper">
+
+      <div className="section__grid">
+        <a target="_blank" rel="noreferrer" href="https://docs.google.com/document/d/1cMBilsXrNDPjtWA8AdLMk4fNb3YKX8Eg/edit?usp=sharing&ouid=104991853798134703900&rtpof=true&sd=true">
+          <div className="section__box">
+            <div className="section__content">
+              <h3>Клікни, щоби читати</h3>
+
+              <p>Телефони гарячих ліній</p>
+            </div>
+          </div>
+        </a>
+      </div>
+
       <div className="section__grid">
         <a target="_blank" rel="noreferrer" href="https://www.youtube.com/watch?v=6m8YK6vldYI&app=desktop">
           <div className="section__box">
