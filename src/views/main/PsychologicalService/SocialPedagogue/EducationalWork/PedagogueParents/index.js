4406bde3b69e88c298b5878248c5f892ea8be075
stror10 <str.or10@gmail.com>
2024-11-15 16:46:14 +0200

    Social pedagogue update

--- src/views/main/PsychologicalService/SocialPedagogue/EducationalWork/PedagogueParents/index.js
+++ src/views/main/PsychologicalService/SocialPedagogue/EducationalWork/PedagogueParents/index.js
@@ -6,6 +6,26 @@ const PedagogueParents = () => {
       <div className="article__wrapper">
       <div className="section__grid">
 
+      <a target="_blank" rel="noreferrer" href="https://docs.google.com/document/d/1ra6HPNttVqGmw1sdt5U4yO7AmAR-plnB/edit?usp=sharing&ouid=104991853798134703900&rtpof=true&sd=true">
+          <div className="section__box">
+            <div className="section__content">
+              <h3>Клікни, щоби читати</h3>
+
+              <p>Ризики та наслідки вживання ПАР</p>
+            </div>
+          </div>
+        </a>
+
+      <a target="_blank" rel="noreferrer" href="https://docs.google.com/document/d/1CFLaENJR7VglAf27eJ0wuJwp_v2XQDNk/edit?usp=sharing&ouid=104991853798134703900&rtpof=true&sd=true">
+          <div className="section__box">
+            <div className="section__content">
+              <h3>Клікни, щоби читати</h3>
+
+              <p>Вплив соціальних мереж на вживання психоактивних речовин серед підлітків</p>
+            </div>
+          </div>
+        </a>
+
       <a target="_blank" rel="noreferrer" href="https://docs.google.com/document/d/1i70bXqKSv-Eay4oZotwAhDq4FzDkSwnQ/edit?usp=sharing&ouid=104991853798134703900&rtpof=true&sd=true">
           <div className="section__box">
             <div className="section__content">
