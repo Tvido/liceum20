577b31712d614d647a9ba8b968ced4481bebd134
stror10 <str.or10@gmail.com>
2025-02-21 18:06:05 +0200

    Rules of Addmission & ServiceTerritory update

--- src/views/main/RulesOfAdmission/index.js
+++ src/views/main/RulesOfAdmission/index.js
@@ -5,17 +5,17 @@ const RulesOfAdmission = () => {
     <React.Fragment>
       <div className="article__wrapper">
       <div className="section__grid">
-        <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1y05jidTkvHnaSHdgYW94uQbdAblh-LUr/view?usp=share_link">
+        <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1sHTSYHUaPiVKs9DTkZaI5Cy1FnaAMzMp/view?usp=sharing">
           <div className="section__box">
             <div className="section__content">
               <h3>Клікни, щоби читати</h3>
 
-              <p>Наказ Департаменту освіти та науки</p>
+              <p>Лист Департаменту освіти та науки</p>
             </div>
           </div>
         </a>
 
-        <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1BM49xL807vVjiQxx7yZJ2TLazvW4uH4v/view?usp=share_link">
+        <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/18y6Q-xJntWUDIswqdACIdTNApHSP6rUi/view?usp=sharing">
           <div className="section__box">
             <div className="section__content">
               <h3>Клікни, щоби читати</h3>
@@ -24,8 +24,8 @@ const RulesOfAdmission = () => {
             </div>
           </div>
         </a>
-
-        <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1x54GPyP3kOJU5NxoFLeKdT2Iezta_k4_/view?usp=share_link">
+      
+        <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1oi63kLwuL0NRJ4UgTcu7GUWkeXZEwNdE/view?usp=sharing">
           <div className="section__box">
             <div className="section__content">
               <h3>Клікни, щоби читати</h3>
@@ -35,7 +35,7 @@ const RulesOfAdmission = () => {
           </div>
         </a>
 
-        <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1_3_D3OMduWjpBonqWLp7_V7lA-dlpyr6/view?usp=share_link">
+        <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1L97XD7BvUTJhrGfQxuAbKUnqmTgcQ9C0/view?usp=sharing">
           <div className="section__box">
             <div className="section__content">
               <h3>Клікни, щоби читати</h3>
--- src/views/main/ServiceTerritory/index.js
+++ src/views/main/ServiceTerritory/index.js
@@ -5,7 +5,7 @@ const ServiceTerritory = () => {
     <React.Fragment>
       <div className="article__wrapper">
       <div className="section__grid">
-        <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1KE8uWcNUmUnOVgGWkdBdksud9FV9q1yE/view?usp=share_link">
+        <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1WOqbiva4YhuQvaqoqRs6d1N0ClO8k14R/view?usp=sharing">
           <div className="section__box">
             <div className="section__content">
               <h3>Клікни, щоби читати</h3>
