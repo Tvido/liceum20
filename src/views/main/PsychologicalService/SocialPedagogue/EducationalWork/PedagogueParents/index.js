7fc952d486351a2317fb3dd5ec80c7738123f374
stror10 <str.or10@gmail.com>
2025-03-15 15:09:37 +0200

    PedagogueParents update

--- src/views/main/PsychologicalService/SocialPedagogue/EducationalWork/PedagogueParents/index.js
+++ src/views/main/PsychologicalService/SocialPedagogue/EducationalWork/PedagogueParents/index.js
@@ -6,6 +6,16 @@ const PedagogueParents = () => {
       <div className="article__wrapper">
       <div className="section__grid">
 
+      <a target="_blank" rel="noreferrer" href="https://docs.google.com/document/d/1cA11cTofh3YzoSQRnPnXa7hS5e8CG3By/edit?usp=sharing&ouid=104991853798134703900&rtpof=true&sd=true">
+          <div className="section__box">
+            <div className="section__content">
+              <h3>Клікни, щоби читати</h3>
+
+              <p>Як розвивати терплячість у дітей та батьків</p>
+            </div>
+          </div>
+        </a>
+
       <a target="_blank" rel="noreferrer" href="https://docs.google.com/document/d/1ra6HPNttVqGmw1sdt5U4yO7AmAR-plnB/edit?usp=sharing&ouid=104991853798134703900&rtpof=true&sd=true">
           <div className="section__box">
             <div className="section__content">
