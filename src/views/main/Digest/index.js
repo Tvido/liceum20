3f5337f39110c4bedbc6074ad949deabc1bd7c27
stror10 <str.or10@gmail.com>
2025-06-17 12:16:33 +0300

    Digest update

--- src/views/main/Digest/index.js
+++ src/views/main/Digest/index.js
@@ -43,7 +43,45 @@ const Digest = () => {
     <React.Fragment>
       <div className="article__wrapper">
 
-              <div className="cardWrapper">
+         <div className="cardWrapper">
+          <Col md={12}>
+            <Card>
+              <Card.Header>
+                <Card.Title as="h5">16.06.2025</Card.Title>
+                
+              </Card.Header>
+              <Card.Body>
+
+              <blockquote>
+                  <div>
+                  <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1gAZ6pjwCDPTPySDTPXtaReQhUVdNT3hR/view?usp=sharing">
+          <div className="section__box">
+            <div className="section__content">
+              <h3>Клікни, щоби читати</h3>
+
+              <p>Про зарахування здобувачів освіти на вільні місця у перші класи 2025-2026 н.р.</p>
+            </div>
+          </div>
+        </a>
+                  </div>
+                </blockquote>
+           
+             
+                
+              <br></br>
+
+             
+                <blockquote className="blockquote text-right">
+                  <footer className="blockquote-footer">
+                  <a href=""><cite title="Source Title">Про зарахування здобувачів освіти на вільні місця у перші класи 2025-2026 н.р.</cite></a>
+                  </footer>
+                </blockquote>
+              </Card.Body>
+            </Card>
+          </Col>
+      </div>
+
+      <div className="cardWrapper">
           <Col md={12}>
             <Card>
               <Card.Header>
