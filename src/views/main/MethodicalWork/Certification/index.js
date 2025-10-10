06a4eb3889557619de4e93bad2c1d4e305b1ac0a
stror <str.or10@gmail.com>
2025-10-10 18:30:11 +0300

    Certification 2026

--- src/views/main/MethodicalWork/Certification/index.js
+++ src/views/main/MethodicalWork/Certification/index.js
@@ -71,12 +71,12 @@ const Certification = () => {
           </div>
         </a>
 
-        <a target="_blank" rel="noreferrer" href=" https://docs.google.com/document/d/1kunql054ACUPJMBAxJJqeZl5fLGz0PSM/edit?usp=sharing&ouid=104991853798134703900&rtpof=true&sd=true">
+        <a target="_blank" rel="noreferrer" href=" https://docs.google.com/document/d/1QJpwyXz-4cFmVNeW40W-hqMjH0iDUGET/edit?usp=sharing&ouid=104991853798134703900&rtpof=true&sd=true">
           <div className="section__box">
             <div className="section__content">
               <h3>Клікни, щоби читати</h3>
 
-              <p>Список педагогічних працівників Ліцею №20, які атестуються у 2025</p>
+              <p>Список педагогічних працівників Ліцею №20, які атестуються у 2026</p>
             </div>
           </div>
         </a>
