d574c75b476b2e34794430416a22284c02038522
stror <str.or10@gmail.com>
2025-11-10 16:51:07 +0200

    Bullying update 2 pages

--- src/views/main/Bullying/DomesticViolence/index.js
+++ src/views/main/Bullying/DomesticViolence/index.js
@@ -7,12 +7,30 @@ const DomesticViolence = () => {
     <React.Fragment>
       <div className="article__wrapper">
       <div className="section__grid">
-        <a target="_blank" rel="noreferrer" href="https://osvita.diia.gov.ua/">
+        <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1o5oKXzywXD_Sp_OSvVUYxQC8NA11wuDR/view?usp=sharing">
           <div className="section__box">
             <div className="section__content">
               <h3>Клікни, щоби читати</h3>
 
-              <p>-----</p>
+              <p>Про запобігання та протидію домашньому насильству</p>
+            </div>
+          </div>
+        </a>
+        <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1WURm7QtVcNanvbAKZi-iaFi0WcUIJbU7/view?usp=sharing">
+          <div className="section__box">
+            <div className="section__content">
+              <h3>Клікни, щоби читати</h3>
+
+              <p>Додаток. План заходів щодо роботи ліцею у сфері протидії домашньому насильству на 2025-2026 навчальний рік</p>
+            </div>
+          </div>
+        </a>
+        <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1Tp9tri895AowYC1BeK0BMpA86Z4DmB3u/view?usp=sharing">
+          <div className="section__box">
+            <div className="section__content">
+              <h3>Клікни, щоби читати</h3>
+
+              <p>Додаток №1. Алгоритм дій у разі виявлення ознак чи фактів, що можуть вказувати на вчинення домашнього насильства щодо дитини</p>
             </div>
           </div>
         </a>
--- src/views/main/Bullying/PreventingAndCombatingViolenceAndAbuseOfChildren/index.js
+++ src/views/main/Bullying/PreventingAndCombatingViolenceAndAbuseOfChildren/index.js
@@ -7,12 +7,48 @@ const PreventingAndCombatingViolenceAndAbuseOfChildren = () => {
     <React.Fragment>
       <div className="article__wrapper">
       <div className="section__grid">
-        <a target="_blank" rel="noreferrer" href="https://osvita.diia.gov.ua/">
+        <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1aQc99ivxTZstiHgiMYVEhFukBGSodcxh/view?usp=sharing">
           <div className="section__box">
             <div className="section__content">
               <h3>Клікни, щоби читати</h3>
 
-              <p>-----</p>
+              <p>Про запобігання і протидії насильству та жорстокому поводженню з дітьми у ліцеї.</p>
+            </div>
+          </div>
+        </a>
+        <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1_fbEfJKCvtPtJLRidjFADs4D36hBHh5D/view?usp=sharing">
+          <div className="section__box">
+            <div className="section__content">
+              <h3>Клікни, щоби читати</h3>
+
+              <p>Додаток. Положення про запобігання і протидії насильству та жорстокому поводженню з дітьми в Ліцеї №20 Івано-Франківської міської ради</p>
+            </div>
+          </div>
+        </a>
+        <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1jhyHVZ1BodnfiQtPgsr3kNYYoIog1lwF/view?usp=sharing">
+          <div className="section__box">
+            <div className="section__content">
+              <h3>Клікни, щоби читати</h3>
+
+              <p>Додаток №1. Форма первинного повідомлення про підозру на випадок насильства щодо дитини Ліцею №20 Івано-Франківської міської ради</p>
+            </div>
+          </div>
+        </a>
+        <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1HLv9FKMtGpBSuqSmgAradfNH4NjXjhfI/view?usp=sharing">
+          <div className="section__box">
+            <div className="section__content">
+              <h3>Клікни, щоби читати</h3>
+
+              <p>Додаток №2. Журнал безпеки (форма реєстрації внутрішньго інциденту)</p>
+            </div>
+          </div>
+        </a>
+        <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1zXxDDdQ4ydMRoobdgD-fU1VbseAzdtvx/view?usp=sharing">
+          <div className="section__box">
+            <div className="section__content">
+              <h3>Клікни, щоби читати</h3>
+
+              <p>Додаток №3. Анкета анонімного опитування для дітей</p>
             </div>
           </div>
         </a>
