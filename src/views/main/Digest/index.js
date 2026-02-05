882226f8c8a4bf947b7da1ac71ffa6b7ef711459
stror10 <str.or10@gmail.com>
2026-02-05 13:24:47 +0200

    Digest update (3)

--- src/views/main/Digest/index.js
+++ src/views/main/Digest/index.js
@@ -43,6 +43,50 @@ const Digest = () => {
     <React.Fragment>
       <div className="article__wrapper">
 
+           <div className="cardWrapper">
+          <Col md={12}>
+            <Card>
+              <Card.Header>
+                <Card.Title as="h5">05.02.2026</Card.Title>
+                
+              </Card.Header>
+              <Card.Body>
+
+              <blockquote>
+                  <div>
+                  <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1WUmpaD_7t91tXT9g7v__gnXQS562nziJ/view?usp=sharing">
+          <div className="section__box">
+            <div className="section__content">
+              <h3>Клікни, щоби читати</h3>
+
+              <p>Про методичні рекомендації щодо способів фільтрації доступу до мережі Інтернет та протидії шкідливому контенту в закладах освіти</p>
+            </div>
+          </div>
+        </a>
+                  </div>
+                </blockquote>
+           
+             
+                
+              <br></br>
+
+             
+                <blockquote className="blockquote text-right">
+                  <footer className="blockquote-footer">
+                  <a href="https://drive.google.com/file/d/1M8wd4g3YHOpeQSy_tm5fq0xEHYzqxoeq/view?usp=sharing"><cite title="Source Title">Лист області для врахування (Про затвердження методичних рекомендації)</cite></a>
+                  </footer>
+                </blockquote>
+
+                <blockquote className="blockquote text-right">
+                  <footer className="blockquote-footer">
+                  <a href="https://drive.google.com/file/d/1TC8U1CfB4qnEI9cK2K2jbK4v0QaE71LA/view?usp=sharing"><cite title="Source Title">Наказ 103 (Про затвердження Методичних рекомендацій щодо способів фільтрації доступу до мережі Інтернет та протидії шкідливому контенту в закладах освіти</cite></a>
+                  </footer>
+                </blockquote>
+              </Card.Body>
+            </Card>
+          </Col>
+      </div>
+
         <div className="cardWrapper">
           <Col md={12}>
             <Card>
