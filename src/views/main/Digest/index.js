465d241b54bf89fd1e51f18103ba4b857a0a62a4
stror10 <str.or10@gmail.com>
2026-02-19 12:23:57 +0200

    Digest (2) supervisia

--- src/views/main/Digest/index.js
+++ src/views/main/Digest/index.js
@@ -43,11 +43,56 @@ const Digest = () => {
     <React.Fragment>
       <div className="article__wrapper">
 
+                          <div className="cardWrapper">
+          <Col md={12}>
+            <Card>
+              <Card.Header>
+                <Card.Title as="h5">19.02.2026</Card.Title>
+                
+              </Card.Header>
+              <Card.Body>
+
+              <blockquote>
+                  <div>
+                  <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1mtC4_pswtsBPjhhYADGQbBRO8n6Ij8m1/view?usp=sharing">
+          <div className="section__box">
+            <div className="section__content">
+              <h3>Клікни, щоби читати</h3>
+
+              <p>Щодо поширення інформаційних матеріалів та підтримку впровадження експериментального проєкту з підвищення кваліфікації та супервізії педагогічних працівників ЗЗСО</p>
+            </div>
+          </div>
+        </a>
+                  </div>
+                </blockquote>
+           
+             
+                
+              <br></br>
+
+             
+                <blockquote className="blockquote text-right">
+                  <footer className="blockquote-footer">
+                  <a href="https://drive.google.com/drive/u/1/folders/1TH4v8mCkHwkl4NOfdFQV_VjgZ8wT1LVf"><cite title="Source Title">Матеріали доступні за посиланням: https://drive.google.com/drive/u/1/folders/1TH4v8mCkHwkl4NOfdFQV_VjgZ8wT1LVf</cite></a>
+                  </footer>
+                </blockquote>
+
+                <blockquote className="blockquote text-right">
+                  <footer className="blockquote-footer">
+                  <a href="https://docs.google.com/document/d/1u16pcc1yZysswTB9GHxtBcpVDlvXE7vXkjuDUhMzbQo/edit?usp=sharing"><cite title="Source Title">Для публікації на сайтах може бути використаний підготовлений шаблон повідомлень, доступний за посиланням: https://docs.google.com/document/d/1u16pcc1yZysswTB9GHxtBcpVDlvXE7vXkjuDUhMzbQo/edit?usp=sharing.</cite></a>
+                  </footer>
+                </blockquote>
+
+              </Card.Body>
+            </Card>
+          </Col>
+      </div>
+
                    <div className="cardWrapper">
           <Col md={12}>
             <Card>
               <Card.Header>
-                <Card.Title as="h5">05.02.2026</Card.Title>
+                <Card.Title as="h5">13.02.2026</Card.Title>
                 
               </Card.Header>
               <Card.Body>
