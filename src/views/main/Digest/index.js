87f0bd3c778a94cb50697073b6e52195a0129866
stror <str.or10@gmail.com>
2026-05-21 14:18:24 +0300

    Digest (1)

--- src/views/main/Digest/index.js
+++ src/views/main/Digest/index.js
@@ -43,6 +43,48 @@ const Digest = () => {
     <React.Fragment>
       <div className="article__wrapper">
 
+              <div className="cardWrapper">
+          <Col md={12}>
+            <Card>
+              <Card.Header>
+                <Card.Title as="h5">21.05.2026</Card.Title>
+                
+              </Card.Header>
+              <Card.Body>
+
+              <blockquote>
+                  <div>
+                  <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1CFZ_6R6j8-7Vv51eJgRHhuIeHIGLBMPH/view?usp=sharing">
+          <div className="section__box">
+            <div className="section__content">
+              <h3>Клікни, щоби читати</h3>
+
+              <p>Презентаційні матеріали для майбутніх випускників (Кібербезпека)</p>
+            </div>
+          </div>
+        </a>
+                  </div>
+                </blockquote>
+           
+             
+                
+              <br></br>
+
+             
+                <blockquote className="blockquote text-right">
+                  <footer className="blockquote-footer">
+                  <a href="https://drive.google.com/file/d/1CFZ_6R6j8-7Vv51eJgRHhuIeHIGLBMPH/view?usp=sharing"><cite title="Source Title">Презентаційні матеріали для майбутніх випускників (Кібербезпека)</cite></a>
+                  </footer>
+                </blockquote>
+
+
+               
+
+              </Card.Body>
+            </Card>
+          </Col>
+      </div>
+
             <div className="cardWrapper">
           <Col md={12}>
             <Card>
