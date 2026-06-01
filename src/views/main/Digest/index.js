17c039830351addf0dd360383f0b60b9a24a42b1
stror <str.or10@gmail.com>
2026-06-01 15:55:57 +0300

    Digest (Order on enrollment in 1st grade)

--- src/views/main/Digest/index.js
+++ src/views/main/Digest/index.js
@@ -43,6 +43,48 @@ const Digest = () => {
     <React.Fragment>
       <div className="article__wrapper">
 
+                <div className="cardWrapper">
+          <Col md={12}>
+            <Card>
+              <Card.Header>
+                <Card.Title as="h5">01.06.2026</Card.Title>
+                
+              </Card.Header>
+              <Card.Body>
+
+              <blockquote>
+                  <div>
+                  <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1bOM0PYkilplnh1bubXF_iJLAWo2HLn2a/view?usp=sharing">
+          <div className="section__box">
+            <div className="section__content">
+              <h3>Клікни, щоби читати</h3>
+
+              <p>Наказ про зарахування здобувачів освіти у перші класи 2026-2027 н.р.</p>
+            </div>
+          </div>
+        </a>
+                  </div>
+                </blockquote>
+           
+             
+                
+              <br></br>
+
+             
+                <blockquote className="blockquote text-right">
+                  <footer className="blockquote-footer">
+                  <a href="https://drive.google.com/file/d/1bOM0PYkilplnh1bubXF_iJLAWo2HLn2a/view?usp=sharing"><cite title="Source Title">Наказ про зарахування здобувачів освіти у перші класи 2026-2027 н.р.</cite></a>
+                  </footer>
+                </blockquote>
+
+
+               
+
+              </Card.Body>
+            </Card>
+          </Col>
+      </div>
+
               <div className="cardWrapper">
           <Col md={12}>
             <Card>
